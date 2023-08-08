aa7153bed123d92b7de36074eccfad65dd0315d0
LewisHammy <samkabir30@gmail.com>
2023-08-07 18:25:38 -0700

    fixed C to F

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function checkWeather(city){
     var data = await response.json();
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " mph";
     document.querySelector(".weather").style.display = "block";
